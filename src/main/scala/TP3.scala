be23963f159e41ce6207dfdca664dba77723638c
ZainZafar <zainzafarsial@gmail.com>
2022-11-26 20:34:30 +0100

    TP2

--- src/main/scala/TP3.scala
+++ src/main/scala/TP3.scala
@@ -75,6 +75,7 @@ object TP3 {
   def gx = e4(x => x)
 
 
+  // Exo 6
 //  En se basant sur le principe de fonctions imbriquées, écrire une fonction
   //calculatrice() qui prend en paramètre deux nombres entiers et effectue les
   //différentes opérations arithmétiques (+ , -, /, % , *) sur ces nombres.
@@ -102,14 +103,102 @@ object TP3 {
 
 
 
+  //Ex7
+  //Ecrire une fonction comparateur() qui prend en paramètre deux listes et vérifie si
+  //elles sont identiques.
+  def comparateur(list1: List[Int], list2: List[Int]): Boolean = {
+    if (list1.size != list2.size)
+      false
+    else {
+      for (i <- list1.indices) {
+        if (list1(i) != list2(i)) return false
+      }
+      true
+    }
+  }
+
+
+  //Ex8
+  //Ecrire une fonction PaireImpaire() composée de deux fonctions imbriquées paire()
+  //et impair(), tel que PaireImpaire() prend en paramètre un tableau d’entiers tab à
+  //deux dimensions, la fonction paire() retourne un tableau à une dimension des
+  //éléments paire de tab et impaire() retourne un autre tableau à une dimension des
+  //éléments impaires de tab
+  def PaireImpaire(tab: Array[Array[Int]]): (Array[Int], Array[Int]) = {
+    def paire: Array[Int] = {
+      var res = Array[Int]()
+      for (i <- tab.indices) {
+        for (j <- tab(i).indices) {
+          if (tab(i)(j) % 2 == 0) res = res :+ tab(i)(j)
+        }
+      }
+      res
+    }
+
+    def impaire: Array[Int] = {
+      var res = Array[Int]()
+      for (i <- tab.indices) {
+        for (j <- tab(i).indices) {
+          if (tab(i)(j) % 2 != 0) res = res :+ tab(i)(j)
+        }
+      }
+      res
+    }
+
+    (paire, impaire)
+  }
+
+  //Ex9
+  //Ecrire une fonction qui prend en paramètre une matrice carrée (n*n) et qui
+  //composée de deux fonctions imbriquées :
+  //La première fonction vérifie si la matrice passée en paramètre est un carré
+  //magique
+  //La deuxième fonction vérifie si la matrice passée en paramètre est un carré
+  //parfait
+
+  def carreMagiqueParfait(tab: Array[Array[Int]]): (Boolean, Boolean) = {
+    def carreMagique: Boolean = {
+      var res = true
+      val somme = tab(0).sum
+      for (i <- tab.indices) {
+        if (tab(i).sum != somme) res = false
+      }
+      res
+    }
+
+    def carreParfait: Boolean = {
+      var res = true
+      val somme = tab(0).sum
+      for (i <- tab.indices) {
+        if (tab(i).sum != somme) res = false
+      }
+      res
+    }
+
+    (carreMagique, carreParfait)
+  }
+
 
   def main(args: Array[String]): Unit = {
-    println(ex1(salaried_map))
-    println(f(1, 5))
-    println(fx(5))
-    println(gx(1, 5))
+//    println(ex1(salaried_map))
+//    println(f(1, 5))
+//    println(fx(5))
+//    println(gx(1, 5))
+
+    //test exo6
     println(calculatrice(5, 5)("+"))
-    println(calculatrice(5, 5)("-"))
+
+    //test exo7
+    println(comparateur(List(1, 2, 3), List(1, 2, 3)))
+
+    //test exo8
+    var z = PaireImpaire(Array(Array(1, 2, 3), Array(4, 5, 6), Array(7, 8, 9)))
+    println("Paire : " + z._1.mkString(", "))
+    println("Impaire : " + z._2.mkString(", "))
+
+
+    //test exo9
+    println(carreMagiqueParfait(Array(Array(2, 7, 6), Array(9, 5, 1), Array(4,3, 8))))
   }
 
 }
